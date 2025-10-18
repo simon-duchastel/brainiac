8b6685970da94894a897b57fe8be26c97221c522
simon-duchastel <41980219+simon-duchastel@users.noreply.github.com>
2025-10-18 03:07:07 -0400

    fix: updating long term memory gets short term memory as input (#38)

--- core-loop/src/jvmMain/kotlin/memory/ShortTermMemory.kt
+++ core-loop/src/jvmMain/kotlin/memory/ShortTermMemory.kt
@@ -4,19 +4,25 @@ import ai.koog.agents.core.dsl.builder.AIAgentBuilderDslMarker
 import ai.koog.agents.core.dsl.builder.AIAgentNodeDelegate
 import ai.koog.agents.core.dsl.builder.AIAgentSubgraphBuilderBase
 import ai.koog.agents.features.tokenizer.feature.tokenizer
+import ai.koog.prompt.dsl.prompt
 
 @AIAgentBuilderDslMarker
 fun AIAgentSubgraphBuilderBase<*, *>.recallShortTermMemory(
     name: String? = null,
 ): AIAgentNodeDelegate<Unit, ShortTermMemory> = node(name) {
-    ShortTermMemory("")
+    ShortTermMemory("TODO")
 }
 
 @AIAgentBuilderDslMarker
 inline fun <reified T> AIAgentSubgraphBuilderBase<*, *>.updateShortTermMemory(
     name: String? = null,
+    tokenThreshold: Int = 50_000,
 ): AIAgentNodeDelegate<T, T> = node(name) { input ->
     val currentTokens = tokenizer.tokenCountFor(llm.prompt)
+
+    if (currentTokens > tokenThreshold) {
+
+    }
 
     input
 }
