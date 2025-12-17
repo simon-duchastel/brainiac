dca3176803c4ebdb0ae1fe30df1f3e7bcf8dffaf
Simon Duchastel <simon@duchastel.com>
2025-12-16 23:14:58 -0500

    feat: Re-implement Kotlin Native changes for CLI module

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,27 +1,41 @@
 plugins {
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.compiler)
-    application
 }
 
 group = "com.duchastel.simon.brainiac.cli"
 
-application {
-    mainClass.set("com.duchastel.simon.brainiac.cli.MainKt")
-}
-
 kotlin {
-    jvmToolchain(17)
-}
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    linuxX64("native") {
+        binaries {
+            executable {
+                entryPoint = "com.duchastel.simon.brainiac.cli.main"
+            }
+        }
+    }
 
-dependencies {
-    implementation(project(":core-agent"))
-    implementation(project(":tools"))
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.okio)
-    implementation(libs.slf4j.nop)
-    implementation(libs.mosaic.runtime)
-    implementation(libs.circuit.foundation)
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":core-agent"))
+                implementation(project(":tools"))
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.okio)
+                implementation(libs.circuit.foundation)
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.slf4j.nop)
+                implementation(libs.mosaic.runtime)
+            }
+        }
+        val nativeMain by getting
+    }
 }
 
 tasks.withType<JavaExec> {
